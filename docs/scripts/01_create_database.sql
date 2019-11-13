ce7814e524c2cce50b30a9549e05105542fe3108
Mauricio Gomez <mauriciogotze.9@yahoo.com>
2019-11-12 22:22:25 -0600

    segundo commit
    creacion de base de datos he identidad

--- docs/scripts/01_create_database.sql
+++ docs/scripts/01_create_database.sql
@@ -1,3 +1,3 @@
-CREATE SCHEMA `examen` ;
-CREATE USER 'examen'@'127.0.0.1' IDENTIFIED BY 'essenHypB4nk';
-GRANT ALL ON examen.* TO 'examen'@'127.0.0.1';
+CREATE SCHEMA `dinosaurios` ;
+CREATE USER 'dinosaurios'@'127.0.0.1' IDENTIFIED WITH mysql_native_password BY '12345';
+GRANT ALL ON dinosaurios.* TO 'dinosaurios'@'127.0.0.1';
---
+++ docs/scripts/02_createTable_dinosaurs.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `dinosaurios`(
+  `iddinosaurios` BIGINT(500) NOT NULL,
+  `dinosauriosNombre` VARCHAR(60) NULL,
+  `dinosauriosPeso` DECIMAL(10,2) NULL,
+  `dinosauriosClase` VARCHAR(60) NULL,
+  `dinosauriosEpoca` CHAR(3) NULL,
+  PRIMARY KEY (`iddinosaurios`));
